10719a94565f006bad8077cca8d496e8fc56e573
sushilmate <sushil.mate@outlook.com>
2020-06-11 20:34:33 +0200

    Adding login design details.

--- InnoscriptaPizza/ClientApp/src/app/login/login.component.ts
+++ InnoscriptaPizza/ClientApp/src/app/login/login.component.ts
@@ -6,18 +6,43 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
-  public forecasts: WeatherForecast[];
+  //public forecasts: WeatherForecast[];
+  public orderDetails: OrderHistory[] = [];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+    //http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
+    //  this.forecasts = result;
+    //}, error => console.error(error));
+  }
+
+  onLogin() {
+    alert("User Logged in Successfully.");
+
+    const order = new OrderHistory();
+    order.date = "12-Jan-2020";
+    order.pizzaName = "Pepe Paneer";
+    order.quantity = 2;
+    order.amount = 50;
+    order.status = "Delievered"
+
+    this.orderDetails.push(order);
+
+
+    const order1 = new OrderHistory();
+    order1.date = "17-Feb-2020";
+    order1.pizzaName = "Chicken Masala";
+    order1.quantity = 4;
+    order1.amount = 120;
+    order1.status = "Delievered"
+
+    this.orderDetails.push(order1);
   }
 }
 
-interface WeatherForecast {
+export class OrderHistory {
   date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+  pizzaName: string;
+  quantity: number;
+  amount: number;
+  status: string;
 }
