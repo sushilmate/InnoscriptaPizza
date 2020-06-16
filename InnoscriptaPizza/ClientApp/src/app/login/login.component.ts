8a93798162ff2c3b32b4507551596fc38b0adaaf
sushilmate <sushil.mate@outlook.com>
2020-06-16 15:56:37 +0200

    bug fix.

--- InnoscriptaPizza/ClientApp/src/app/login/login.component.ts
+++ InnoscriptaPizza/ClientApp/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { OrderDetails } from '../order-deails/shared/order-details.model';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -14,13 +15,21 @@ export class LoginComponent {
   public userName: string;
 
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string,
-    private spinner: NgxSpinnerService) {
+    private spinner: NgxSpinnerService, private toastrService: ToastrService) {
+    this.userName = '';
   }
 
   onLogin() {
     this.spinner.show();
     this.http.get<OrderDetails[]>(this.baseUrl + 'OrderDetails/' + this.userName.toLowerCase()).subscribe(result => {
       this.orderDetails = result;
-    }, error => console.error(error), () => { this.spinner.hide() });
+      if (this.orderDetails.length === 0)
+        this.toastrService.error("No order history found under user: " + this.userName, "", {
+          timeOut: 2000
+        });
+    }, error => {
+        console.error(error);
+        alert(error);
+    }, () => { this.spinner.hide() });
   }
 }
